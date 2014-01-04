703b8299fbcc8bfbb0036670faa5e2f404f5117b
Sven Lito <svnlto@users.noreply.github.com>
2014-01-04 21:41:13 +0100

    Update backbone-hoodie.js

--- www/libs/backbone-hoodie.js
+++ www/libs/backbone-hoodie.js
@@ -1,4 +1,4 @@
-(function (root, factory) {
+(function(root, factory) {
 
   'use strict';
 
@@ -18,41 +18,50 @@
     root.Backbone.hoodie = factory(root.Backbone, root.Hoodie);
   }
 
-
-}(this, function (Backbone, Hoodie) {
+}(this, function(Backbone, Hoodie) {
 
   'use strict';
 
-  Backbone.connect = function (url) {
+  Backbone.connect = function(url) {
     Backbone.hoodie = new Hoodie(url);
   };
 
-  Backbone.sync = function (method, modelOrCollection, options) {
+  Backbone.sync = function(method, modelOrCollection, options) {
     var attributes, id, promise, type;
 
     id = modelOrCollection.id;
     attributes = modelOrCollection.attributes;
     type = modelOrCollection.type;
 
-    type = type || (type = modelOrCollection.model.prototype.type);
+    if (!type) {
+      type = modelOrCollection.model.prototype.type;
+    }
 
-    promise = (function () {
-      switch (method) {
-        case 'read':
-          if (id) {
-            return Backbone.hoodie.store.find(type, id);
-          } else {
-            return Backbone.hoodie.store.findAll(type);
-          }
-          break;
-        case 'create':
-          return Backbone.hoodie.store.add(type, attributes);
-        case 'update':
-          return Backbone.hoodie.store.update(type, id, attributes);
-        case 'delete':
-          return Backbone.hoodie.store.remove(type, id);
+    switch (method) {
+    case 'read':
+      if (id) {
+        promise = Backbone.hoodie.store.find(type, id);
+      } else {
+        if (options.filter) {
+          promise = Backbone.hoodie.store.findAll(options.filter);
+        } else {
+          promise = Backbone.hoodie.store.findAll(type);
+        }
       }
-    })();
+      break;
+    case 'create':
+      promise = Backbone.hoodie.store.add(type, attributes, options).done(function(attributes) {
+        modelOrCollection.set(attributes);
+      });
+      break;
+    case 'update':
+      promise = Backbone.hoodie.store.updateOrAdd(type, id, attributes).done(function(attributes) {
+        modelOrCollection.set(attributes);
+      });
+      break;
+    case 'delete':
+      promise = Backbone.hoodie.store.remove(type, id);
+    }
 
     if (options.success) {
       promise.done(options.success);
@@ -62,73 +71,72 @@
       return promise.fail(options.error);
     }
 
+    // allow for chaining
+    return promise;
   };
 
-  Backbone.Model.prototype.merge = function (attributes) {
+  Backbone.Model.prototype.merge = function(attributes) {
     this.set(attributes, {
       remote: true
     });
   };
 
-  Backbone.Collection.prototype.initialize = function () {
+  Backbone.Collection.prototype.initialize = function() {
     var type;
     var self = this;
+    var store;
 
-    if (this.model) {
-      type = this.model.prototype.type;
-
-      this.fetch();
-
-      if (type) {
-
-        Backbone.hoodie.store.on('add:' + type, function (attributes) {
-          self.eventAdd(attributes);
-        });
-
-        Backbone.hoodie.remote.on('add:' + type, function (attributes) {
-          self.eventAdd(attributes);
-        });
-
-        Backbone.hoodie.store.on('remove:' + type, function (attributes) {
-          self.eventRemove(attributes);
-        });
-
-        Backbone.hoodie.remote.on('remove:' + type, function (attributes) {
-          self.eventRemove(attributes);
-        });
-
-        Backbone.hoodie.store.on('update:' + type, function (attributes) {
-          self.eventUpdate(attributes);
-        });
-
-        Backbone.hoodie.remote.on('update:' + type, function (attributes) {
-          self.eventUpdate(attributes);
-        });
-
-      }
-
+    if (!this.model) {
+      return;
     }
 
-  };
+    type = this.model.type;
 
-  Backbone.Collection.prototype.eventAdd = function (attributes) {
-    this.add(attributes);
-  };
+    if (type) {
 
-  Backbone.Collection.prototype.eventRemove = function (attributes) {
-    var id, _ref;
-    id = attributes.id;
+      store = Backbone.hoodie.store(type);
+      store.on('add', function(attributes, options) {
+        var record;
 
-    return (_ref = this.get(id)) !== null ? _ref.destroy() : void 0;
-  };
+        // see https://github.com/hoodiehq/backbone-hoodie/issues/3
+        if (self.storeBindingFilter && !self.storeBindingFilter(attributes)) {
+          return;
+        }
 
-  Backbone.Collection.prototype.eventUpdate = function (attributes) {
-    var id, _ref;
-    id = attributes.id;
-    return (_ref = this.get(id)) !== null ? _ref.merge(attributes) : void 0;
+        record = self.add(attributes, options);
+        self.trigger('create', record, options);
+      });
+
+      store.on('remove', function(attributes, options) {
+        var record;
+
+        if (self.storeBindingFilter && !self.storeBindingFilter(attributes)) {
+          return;
+        }
+
+        record = self.get(attributes.id);
+        if (record) {
+          record.destroy(options);
+        }
+      });
+
+      store.on('update', function(attributes, options) {
+        var record;
+
+        if (self.storeBindingFilter && !self.storeBindingFilter(attributes)) {
+          return;
+        }
+
+        record = self.get(attributes.id);
+        if (options.remote && record) {
+          record.merge(attributes);
+        }
+
+        self.trigger('update', record, options);
+      });
+    }
   };
 
   return Backbone;
 
 }));
-
