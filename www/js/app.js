eaf7083f70dab0d84d5e027d320f49d4f3d56d11
Sven Lito <me@svenlito.com>
2015-01-15 23:40:05 +0100

    refact(misc): update template

--- www/js/app.js
+++ www/js/app.js
@@ -1,309 +1,216 @@
-/** @jsx React.DOM */
+var hoodie = new global.Hoodie();
+var React = require('react');
+var Fluxxor = require('fluxxor');
 
-// backbone hoodie connector go!
-Backbone.hoodie.connect();
-
-var Todo = Backbone.Model.extend({
-
-  // backbone-hoodie needs this
-  type: 'todo',
-  
-  // Default attributes for the todo
-  // and ensure that each todo created has `title` and `completed` keys.
-  defaults: {
-    title: '',
-    completed: false
-  },
-
-  // Toggle the `completed` state of this todo item.
-  toggle: function() {
-    this.save({
-      completed: !this.get('completed')
-    });
-  }
-});
-
-var TodoList = Backbone.Collection.extend({
-
-  // Reference to this collection's model.
-  model: Todo,
-
-  // Filter down the list of all todo items that are finished.
-  completed: function() {
-    return this.filter(function( todo ) {
-      return todo.get('completed');
-    });
-  },
-
-  remaining: function() {
-    return this.without.apply(this, this.completed());
-  },
-
-  // We keep the Todos in sequential order, despite being saved by unordered
-  // GUID in the database. This generates the next order number for new items.
-  nextOrder: function () {
-    if (!this.length) {
-      return 1;
-    }
-    return this.last().get('order') + 1;
-  },
-
-  // Todos are sorted by their original insertion order.
-  comparator: function (todo) {
-    return todo.get('order');
-  }
-});
-
-var Utils = {
-  pluralize: function( count, word ) {
-    return count === 1 ? word : word + 's';
-  },
-
-  stringifyObjKeys: function(obj) {
-    var s = '';
-    for (var key in obj) {
-      if (!obj.hasOwnProperty(key)) {
-        continue;
-      }
-      if (obj[key]) {
-        s += key + ' ';
-      }
-    }
-    return s;
-  }
+var constants = {
+  LOAD: 'LOAD',
+  LOAD_SUCCESS: 'LOAD_SUCCESS',
+  LOAD_ERROR: 'LOAD_ERROR',
+  ADD_TODO: 'ADD_TODO',
+  TOGGLE_TODO: 'TOGGLE_TODO',
+  CLEAR_TODOS: 'CLEAR_TODOS'
 };
 
-// Begin React stuff
+var TodoStore = Fluxxor.createStore({
+  initialize: function () {
+    this.loading = false;
+    this.error = null;
+    this.todos = [];
 
-var TodoItem = React.createClass({
-  handleSubmit: function(event) {
-    var val = this.refs.editField.getDOMNode().value.trim();
-    if (val) {
-      this.props.onSave(val);
-    } else {
-      this.props.onDestroy();
-    }
-    return false;
-  },
-
-  onEdit: function() {
-    this.props.onEdit();
-    this.refs.editField.getDOMNode().focus();
-  },
-
-  render: function() {
-    var classes = Utils.stringifyObjKeys({
-      completed: this.props.todo.get('completed'), editing: this.props.editing
-    });
-    return (
-      <li className={classes}>
-        <div className="view">
-          <input
-            className="toggle"
-            type="checkbox"
-            checked={this.props.todo.get('completed')}
-            onChange={this.props.onToggle}
-            key={this.props.key}
-          />
-          <label onDoubleClick={this.onEdit}>
-            {this.props.todo.get('title')}
-          </label>
-          <button className="destroy" onClick={this.props.onDestroy} />
-        </div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            ref="editField"
-            className="edit"
-            defaultValue={this.props.todo.get('title')}
-            onBlur={this.handleSubmit}
-            autoFocus="autofocus"
-          />
-        </form>
-      </li>
+    this.bindActions(
+      constants.LOAD, this.onLoad,
+      constants.LOAD_SUCCESS, this.onLoadSuccess,
+      constants.LOAD_ERROR, this.onLoadError,
+      constants.ADD_TODO, this.onAddTodo,
+      constants.TOGGLE_TODO, this.onToggleTodo,
+      constants.CLEAR_TODOS, this.onClearTodos
     );
-  }
-});
-
-var TodoFooter = React.createClass({
-  render: function() {
-    var activeTodoWord = Utils.pluralize(this.props.count, 'todo');
-    var clearButton = null;
-
-    if (this.props.completedCount > 0) {
-      clearButton = (
-        <button id="clear-completed" onClick={this.props.onClearCompleted}>
-          Clear completed ({this.props.completedCount})
-        </button>
-      );
-    }
-
-    return (
-      <footer id="footer">
-        <span id="todo-count">
-          <strong>{this.props.count}</strong>{' '}
-          {activeTodoWord}{' '}left
-        </span>
-        {clearButton}
-      </footer>
-    );
-  }
-});
-
-// An example generic Mixin that you can add to any component that should react
-// to changes in a Backbone component. The use cases we've identified thus far
-// are for Collections -- since they trigger a change event whenever any of
-// their constituent items are changed there's no need to reconcile for regular
-// models. One caveat: this relies on getBackboneModels() to always return the
-// same model instances throughout the lifecycle of the component. If you're
-// using this mixin correctly (it should be near the top of your component
-// hierarchy) this should not be an issue.
-var BackboneMixin = {
-  componentDidMount: function() {
-    // Whenever there may be a change in the Backbone data, trigger a reconcile.
-    this.getBackboneModels().forEach(function(model) {
-      model.on('add change remove', this.forceUpdate.bind(this, null), this);
-    }, this);
   },
 
-  componentWillUnmount: function() {
-    // Ensure that we clean up any dangling references when the component is
-    // destroyed.
-    this.getBackboneModels().forEach(function(model) {
-      model.off(null, null, this);
-    }, this);
-  }
-};
-
-var TodoApp = React.createClass({
-  mixins: [BackboneMixin],
-  getInitialState: function() {
-    return {editing: null};
+  onLoad: function (payload) {
+    this.loading = true;
+    this.emit('change');
   },
 
-  componentDidMount: function() {
-    // Additional functionality for todomvc: fetch() the collection on init
-    this.props.todos.fetch();
-    this.refs.newField.getDOMNode().focus();
+  onLoadSuccess: function (payload) {
+    this.loading = false;
+    this.error = false;
+    this.todos = payload.todos;
+    this.emit('change');
   },
 
-  componentDidUpdate: function() {
-    // If saving were expensive we'd listen for mutation events on Backbone and
-    // do this manually. however, since saving isn't expensive this is an
-    // elegant way to keep it reactively up-to-date.
-    this.props.todos.forEach(function(todo) {
-      todo.save();
-    });
+  onLoadError: function (payload) {
+    this.loading = false;
+    this.error = payload.err;
+    this.emit('change');
   },
 
-  getBackboneModels: function() {
-    return [this.props.todos];
-  },
-
-  handleSubmit: function(event) {
-    event.preventDefault();
-    var val = this.refs.newField.getDOMNode().value.trim();
-    if (val) {
-      this.props.todos.create({
-        title: val,
-        completed: false,
-        order: this.props.todos.nextOrder()
+  onAddTodo: function (payload) {
+    hoodie.store.add('todos', {
+      text: payload.text,
+      complete: false
+    })
+    .done(function (docs) {
+      this.todos.push({
+        text: payload.text,
+        complete: false
       });
-      this.refs.newField.getDOMNode().value = '';
-    }
+      this.emit('change');
+    }.bind(this));
   },
 
-  toggleAll: function(event) {
-    var checked = event.nativeEvent.target.checked;
-    this.props.todos.forEach(function(todo) {
-      todo.set('completed', checked);
+  onToggleTodo: function (payload) {
+    var todo = payload.todo;
+    todo.complete = !todo.complete;
+
+    hoodie.store.update('todos', todo.id, todo)
+    .done(function (doc) {
+      this.emit('change');
+    }.bind(this));
+  },
+
+  onClearTodos: function () {
+    this.todos.forEach(function (todo, index) {
+      if (todo.complete && this.todos[index].id === todo.id) {
+        delete this.todos[index];
+        hoodie.store.remove('todos', todo.id)
+        .done(function () {
+           this.emit('change');
+        }.bind(this));
+      }
+    }.bind(this));
+  },
+
+  getState: function () {
+    return {
+      todos: this.todos
+    };
+  }
+});
+
+var actions = {
+  loadTodos: function () {
+    this.dispatch(constants.LOAD, {
+      todos: []
     });
+    hoodie.store.findAll()
+    .then(function (docs) {
+      this.dispatch(constants.LOAD_SUCCESS, {
+        todos: docs
+      });
+    }.bind(this))
+    .fail(function (err) {
+      this.dispatch(constants.LOAD_ERROR, {
+        err: err
+      });
+    }.bind(this));
+  },
+  addTodo: function (text) {
+    this.dispatch(constants.ADD_TODO, {text: text});
   },
 
-  edit: function(todo) {
-    this.setState({editing: todo.get('id')});
+  toggleTodo: function (todo) {
+    this.dispatch(constants.TOGGLE_TODO, {todo: todo});
   },
 
-  save: function(todo, text) {
-    todo.set('title', text);
-    this.setState({editing: null});
+  clearTodos: function () {
+    this.dispatch(constants.CLEAR_TODOS);
+  }
+};
+
+var stores = {
+  TodoStore: new TodoStore()
+};
+
+var flux = new Fluxxor.Flux(stores, actions);
+
+flux.on('dispatch', function (type, payload) {
+  if (console && console.log) {
+    console.log('[Dispatch]', type, payload);
+  }
+});
+
+var FluxMixin = Fluxxor.FluxMixin(React),
+    StoreWatchMixin = Fluxxor.StoreWatchMixin;
+
+var Application = React.createClass({
+  mixins: [FluxMixin, StoreWatchMixin('TodoStore')],
+
+  getInitialState: function () {
+    return { newTodoText: '' };
   },
 
-  clearCompleted: function() {
-    this.props.todos.completed().forEach(function(todo) {
-      todo.destroy();
-    });
+  getStateFromFlux: function () {
+    var flux = this.getFlux();
+    // Our entire state is made up of the TodoStore data. In a larger
+    // application, you will likely return data from multiple stores, e.g.:
+    //
+    //   return {
+    //     todoData: flux.store('TodoStore').getState(),
+    //     userData: flux.store('UserStore').getData(),
+    //     fooBarData: flux.store('FooBarStore').someMoreData()
+    //   };
+    return flux.store('TodoStore').getState();
   },
 
-  render: function() {
-    var footer = null;
-    var main = null;
-    var todoItems = this.props.todos.map(function(todo) {
-      return (
-        <TodoItem
-          key={Math.random()}
-          todo={todo}
-          onToggle={todo.toggle.bind(todo)}
-          onDestroy={todo.destroy.bind(todo)}
-          onEdit={this.edit.bind(this, todo)}
-          editing={this.state.editing === todo.get('id')}
-          onSave={this.save.bind(this, todo)}
-        />
-      );
-    }, this);
+  componentDidMount: function () {
+    flux.actions.loadTodos();
+  },
 
-    var activeTodoCount = this.props.todos.remaining().length;
-    var completedCount = todoItems.length - activeTodoCount;
-    if (activeTodoCount || completedCount) {
-      footer =
-        <TodoFooter
-          count={activeTodoCount}
-          completedCount={completedCount}
-          onClearCompleted={this.clearCompleted}
-        />;
-    }
-
-    if (todoItems.length) {
-      main = (
-        <section id="main">
-          <input id="toggle-all" type="checkbox" onChange={this.toggleAll} />
-          <ul id="todo-list">
-            {todoItems}
-          </ul>
-        </section>
-      );
-    }
-
+  render: function () {
     return (
       <div>
-        <section id="todoapp">
-          <header id="header">
-            <h1>todos</h1>
-            <form onSubmit={this.handleSubmit}>
-              <input
-                ref="newField"
-                id="new-todo"
-                placeholder="What needs to be done?"
-              />
-            </form>
-          </header>
-          {main}
-          {footer}
-        </section>
-        <footer id="info">
-          <p>Double-click to edit a todo</p>
-          <p>
-            Created by{' '}
-            <a href="http://github.com/svnlto/">svnlto</a>
-            based on the work of
-            <a href="http://github.com/petehunt/">petehunt</a>
-          </p>
-          <p>Part of{' '}<a href="http://todomvc.com">TodoMVC</a></p>
-        </footer>
+        <ul>
+          {this.state.todos.map(function (todo, i) {
+            return <li key={i}><TodoItem todo={todo} /></li>;
+          })}
+        </ul>
+        <form onSubmit={this.onSubmitForm}>
+          <input type='text' size='30' placeholder='New Todo'
+                 value={this.state.newTodoText}
+                 onChange={this.handleTodoTextChange} />
+          <input type='submit' value='Add Todo' />
+        </form>
+        <button onClick={this.clearCompletedTodos}>Clear Completed</button>
       </div>
     );
+  },
+
+  handleTodoTextChange: function (e) {
+    this.setState({newTodoText: e.target.value});
+  },
+
+  onSubmitForm: function (e) {
+    e.preventDefault();
+    if (this.state.newTodoText.trim()) {
+      this.getFlux().actions.addTodo(this.state.newTodoText);
+      this.setState({newTodoText: ''});
+    }
+  },
+
+  clearCompletedTodos: function (e) {
+    this.getFlux().actions.clearTodos();
   }
 });
 
-React.renderComponent(
-  <TodoApp todos={new TodoList()} />, document.getElementById('container')
-);
+var TodoItem = React.createClass({
+  mixins: [FluxMixin],
+
+  propTypes: {
+    todo: React.PropTypes.object.isRequired
+  },
+
+  render: function () {
+    var style = {
+      textDecoration: this.props.todo.complete ? 'line-through' : ''
+    };
+
+    return <span style={style} onClick={this.onClick}>{this.props.todo.text}</span>;
+  },
+
+  onClick: function () {
+    this.getFlux().actions.toggleTodo(this.props.todo);
+  }
+});
+
+React.render(<Application flux={flux} />, document.getElementById('app'));
